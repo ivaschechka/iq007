c842286c1c948cd21c2ad83aeaf15f2aa641a906
Ivan Antonov <ivan_antonov@live.ru>
2017-03-21 21:31:15 +0300

    Разбор для оплаты

--- iq007/View/AddPayPage.xaml.cs
+++ iq007/View/AddPayPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iq007.Model;
+using Syncfusion.Windows.Shared;
 
 namespace iq007.View
 {
@@ -20,9 +23,30 @@ namespace iq007.View
     /// </summary>
     public partial class AddPayPage : Page
     {
+        ApplicationContext db;
+
         public AddPayPage()
         {
             InitializeComponent();
+            db = new ApplicationContext();
+            db.Rates.Load();
+            db.Payments.Load();
+            comboBox1.ItemsSource = db.Rates.Local.ToBindingList();
+        }
+
+
+        private void Paybutton_Click(object sender, RoutedEventArgs e)
+        {
+            var pupil = comboBoxName.SelectedItem as Pupil;
+            var rate = comboBox1.SelectedItem as Rate;
+            var col = textBox.Text.ConvertToInt64Null();
+            var sel = from pay in db.Payments.Local.ToList()
+                      where (pay.Pupil.Id == pupil.Id && pay.Rate.Id == rate.Id)
+                      select pay;
+            foreach (var payment in sel)
+            {
+                MessageBox.Show("YES");
+            }
         }
     }
 }
